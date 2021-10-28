f83487a8a7b603a7724ed6da1dc232ef7c527376
Bleudywind <thomas.ferrier@edu.ece.fr>
2021-10-28 14:14:04 +0200

    V1.0 fix

--- installOmzsh.sh
+++ installOmzsh.sh
@@ -1,4 +1,5 @@
 zsh --version ||
       	sudo apt install zsh &&
        	chsh -s $(which zsh) &&
+	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 	cp ./.zshrc ~ 
