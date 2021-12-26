495bd29af0a0b50d110efbaffedb7582907a757d
Bleudywind <thom.ferrier@gmail.com>
2021-12-26 15:21:35 +0100

    update command on android

--- installOmzsh.sh
+++ installOmzsh.sh
@@ -1,5 +1,7 @@
 zsh --version ||
-      	sudo apt install -y zsh &&
+      	pkg install -y zsh &&
        	chsh -s $(which zsh) &&
 	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 	cp ./.zshrc ~ 
+
+
